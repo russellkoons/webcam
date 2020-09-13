ffe8a7fa162b636bde55e70f7b949c4916ce3ce7
russellkoons <rkoons@gmail.com>
2020-09-13 07:16:10 -0400

    takePhoto

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+// const snap = document.querySelector('.snap');
 
 function getVid() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
@@ -25,4 +25,21 @@ function paintToCanvas() {
   }, 16)
 }
 
+function takePhoto() {
+  // snap.currentTime = 0;
+  // snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Ugly bwoy" />`
+  strip.insertBefore(link, strip.firstChild);
+}
+
+
+
 getVid();
+
+video.addEventListener('canplay', paintToCanvas);
