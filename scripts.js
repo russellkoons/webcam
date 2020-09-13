e4f8d0f5178ef957a092dd06af630a002cd36a09
russellkoons <rkoons@gmail.com>
2020-09-13 07:28:56 -0400

    rgbSplit

--- scripts.js
+++ scripts.js
@@ -25,7 +25,9 @@ function paintToCanvas() {
     // take out pixels
     let pixels = ctx.getImageData(0, 0, width, height);
     // mess with them
-    pixels = redEffect(pixels);
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1;
     // put them back
     ctx.putImageData(pixels, 0, 0)
   }, 16)
@@ -49,6 +51,16 @@ function redEffect(px) {
     px.data[i + 0] = px.data[i + 0] + 100 // RED
     px.data[i + 1] = px.data[i + 1] - 50 // GREEN
     px.data[i + 2] = px.data[i + 2] * 0.5 // BLUE
+  }
+
+  return px;
+}
+
+function rgbSplit(px) {
+  for (let i = 0; i < px.data.length; i += 4) {
+    px.data[i - 150] = px.data[i + 0] // RED
+    px.data[i + 100] = px.data[i + 1] // GREEN
+    px.data[i - 150] = px.data[i + 2] // BLUE
   }
 
   return px;
