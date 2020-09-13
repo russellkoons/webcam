5c7ce4efac80c6200bbdf0e9640942b66f3f8b9d
russellkoons <rkoons@gmail.com>
2020-09-13 07:01:18 -0400

    webcam working

--- scripts.js
+++ scripts.js
@@ -4,3 +4,13 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
+function getVid() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(mediaStream => {
+      console.log(mediaStream);
+      video.srcObject = mediaStream;
+      video.play();
+    });
+}
+
+getVid();
